ea04d92ad22f10bb1880c2e1f268458e8a812ce6
Richard Tichý <tichy@artify.zone>
2023-03-19 17:07:50 +0100

    Implement solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,24 @@
+// solution for https://codeforces.com/contest/1807/problem/D
 fun main() {
-    println("Hello World!")
+    for (t in (1..readln().toInt())) {
+        val (_, q) = readln().split(' ').map { it.toInt() }
+        val a: List<Triple<Int, Boolean, Boolean>> = run {
+            var tmp = false
+            val a = readln().split(' ').map { it.toInt() }
+                .map { tmp = if(it % 2 == 0) tmp else !tmp; Pair(it, tmp) }
+            tmp = false
+            a.reversed()
+                .map { (it, fromStart) -> tmp = if(it % 2 == 0) tmp else !tmp; Triple(it, fromStart, tmp) }
+                .reversed()
+        }
+
+        for(qi in (1..q)) {
+            val (l, r, k) = readln().split(' ').map { it.toInt() }
+            val fromStart = a.getOrNull(l - 2)?.second ?: false
+            val fromEnd = a.getOrNull(r)?.third ?: false
+            val ends = fromStart != fromEnd
+            val inMiddle = (k % 2 != 0 && (r - l) % 2 == 0)
+            println(if(inMiddle != ends) "YES" else "NO")
+        }
+    }
 }
